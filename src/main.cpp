3a04052e8625ea72afbc92a8170a74344ebd3936
Justin Ho <justinhodev@gmail.com>
2020-06-01 17:51:48 -0700

    Implement File Parsing
    - define constructor and get_score functions
    - add students.cpp to cmake list
    - verify in main with test file (no exceptions / ideal inputs)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "students.h"
 
 int main(int argc, char** argv) {
-  std::cout << "hello world!" << std::endl;
+  Calculator::Students students(argv[1]);
+  students.get_scores();
   return 0;
 }
---
+++ src/students.cpp
@@ -0,0 +1,41 @@
+#include "students.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace Calculator {
+
+  Students::Students(std::string filename) {
+    this->filename = filename;
+  }
+
+  std::vector<int> Students::get_scores() {
+    std::ifstream file;
+    file.open(filename);
+    
+    if (file.is_open()) {
+      std::string name;
+      int score;
+      while (file >> name >> score ) {
+        student_names.push_back(name);
+        scores.push_back(score);
+        // debug output
+        // std::cout << "Student: " + name + " Score: " + std::to_string(score) << std::endl;
+      }
+    }
+
+    return scores;
+  }
+
+  int Students::calculate_average() {
+
+    return average_score;
+  }
+
+  void Students::write_to_file() {
+
+    return;
+  }
+}
